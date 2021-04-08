3f28c3194a843dafdab0a20b86bfdc66a901e3f9
Marek Pętlak <kontakt@marek-petlak.pl>
2021-04-08 17:35:18 +0200

    added user edit form

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,19 +2,52 @@
 
 namespace App\Controller;
 
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/account")
+ */
 class AccountController extends AbstractController
 {
+
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
-     * @Route("/account", name="account")
+     * @Route("", name="account")
      */
     public function show(): Response
     {
-        return $this->render('account/show.html.twig', [
-            'controller_name' => 'AccountController',
+        return $this->render('account/show.html.twig');
+    }
+
+    /**
+     * @Route("/edit", name="account_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request): Response
+    {
+        $userForm = $this->createForm(UserFormType::class, $this->getUser());
+        $userForm->handleRequest($request);
+
+        if ($userForm->isSubmitted() && $userForm->isValid()) {
+            $this->entityManager->flush();
+
+            $this->addFlash('success', 'Profile successfully updated');
+
+            return $this->redirectToRoute('account');
+        }
+
+        return $this->render('account/edit.html.twig', [
+            'form' => $userForm->createView(),
         ]);
     }
 }
